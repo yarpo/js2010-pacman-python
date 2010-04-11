db8d1b5e37c1849eb74b4261e469fe6fe6c35bb5
Jar.Patryk <Jar.Patryk@1bd22912-60a4-6ab0-bb72-b58680a4c78c>
2010-04-11 22:50:45 +0000

    refaktoryzacja poruszania sie + wyswietlania

--- trunk/PyGTKPacman/src/Graphics/Canvas.py
+++ trunk/PyGTKPacman/src/Graphics/Canvas.py
@@ -11,10 +11,12 @@ import math
 
 class Canvas(gtk.DrawingArea):
 
+	_oPlayer  = None;
+	_oEnemies = None;
+	_oCookies = None;
+
 	def __init__(self):
 		gtk.DrawingArea.__init__(self);
-		self.x = 10
-		self.y = 10
 		self.connect("expose-event", self.expose)
 	pass
 
@@ -22,16 +24,59 @@ class Canvas(gtk.DrawingArea):
 		return self.window.cairo_create()
 	pass
 
-	def expose(self, *args):
+	def passPlayer(self, player):
+		self.pl = player;
+		#self.drawPlayer(self.pl)
+	pass
+
+	def _drawPlayer(self):
+		x = self._oPlayer.getX();
+		y = self._oPlayer.getY();
 		cr = self.getContext();
 		cr.set_line_width(4)
-		cr.arc(self.x, self.y, 8, 0, 2 * math.pi)
+		cr.arc(x, y, 8, 0, 2 * math.pi)
 		cr.stroke_preserve()
 		cr.set_source_rgb(1, 1, 1)
 		cr.fill()
 	pass
 
+	def drawPlayer(self, player):
+		self._oPlayer = player;
+	pass
+
+	def drawEnemies(self, enemies):
+		self._oEnemies = enemies;
+	pass
+
+	def _drawEnemies(self):
+		pass
+	pass
+
+	def drawCookies(self, cookies):
+		self._oCookies = cookies;
+	pass
+
+	def _drawCookies(self):
+		pass
+	pass
+
+
+	def expose(self, *args):
+		self._drawPlayer();
+		self._drawEnemies();
+		self._drawCookies();
+		self._reset();
+		return False;
+		#pass
+		#self.drawPlayer();
+	pass
 	
+	def _reset(self):
+		self._oPlayer  = None;
+		self._oEnemies = None;
+		self._oCookies = None;
+	pass
+
 	def draw(self):
 		self.queue_draw();
 	pass
