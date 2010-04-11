93928d07723216e660c431393596b1e476dae10f
Jar.Patryk <Jar.Patryk@1bd22912-60a4-6ab0-bb72-b58680a4c78c>
2010-04-11 17:07:56 +0000

    próby (póki co nieudane) przeniesienia wyrysowywania playera do meotdy playera

--- PyGTKPacman/src/Game/Objects/Character.py
+++ PyGTKPacman/src/Game/Objects/Character.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from Object import *
+
 __version__ = "0.1";
 __author__  = "Patryk yarpo Jar <jar.patryk@gmail.com>";
 __date__    = "$2010-04-11 18:05:03$";
 
 class Character(Object) :
 
-	_iX;
-	_iY;
+	iX = 0;
+	iY = 0;
 
-	def __init(self, x, y):
-		self._iX = x;
-		self._iY = y;
+	def __init__(self, x, y):
+		
+		self.iX = x;
+		self.iY = y;
 	pass
 
 	def draw(self, canvas):
