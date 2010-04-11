d475c830bd278c630be07ce5d5f68b7c2acef935
Jar.Patryk <Jar.Patryk@1bd22912-60a4-6ab0-bb72-b58680a4c78c>
2010-04-11 16:22:37 +0000

    stworzenie pelnego drzewa zaleznosci wsrod obiektów

--- PyGTKPacman/src/Game/Objects/Character.py
+++ PyGTKPacman/src/Game/Objects/Character.py
@@ -1,6 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-__author__="yarpo"
-__date__ ="$2010-04-11 13:18:26$"
+__version__ = "0.1";
+__author__  = "Patryk yarpo Jar <jar.patryk@gmail.com>";
+__date__    = "$2010-04-11 18:05:03$";
 
-if __name__ == "__main__":
-    print "Hello";
+class Character(Object) :
+
+	_iX;
+	_iY;
+
+	def __init(self, x, y):
+		self._iX = x;
+		self._iY = y;
+	pass
+
+	def draw(self, canvas):
+		raise NotImplementedException(ErrorMsg);
+	pass
+
+pass
