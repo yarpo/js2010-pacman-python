54b215fa88c5d52bca4eb88a233158ad77095cde
Jar.Patryk <Jar.Patryk@1bd22912-60a4-6ab0-bb72-b58680a4c78c>
2010-04-11 12:44:16 +0000

    działa drawing area razem z vbox

--- trunk/PyGTKPacman/src/Graphics/Canvas.py
+++ trunk/PyGTKPacman/src/Graphics/Canvas.py
@@ -1,6 +1,59 @@
-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 __author__="yarpo"
 __date__ ="$2010-04-11 13:04:47$"
 
-if __name__ == "__main__":
-    print "Hello";
+import pygtk
+pygtk.require("2.0")
+
+class Canvas(object):
+	'''
+	class responsible for drawing stuff on the screen
+	'''
+	oDrawingArea      = None
+	__width           = None
+	__height          = None
+	__backBuffer      = None
+	__backgroundColor = []
+
+
+	def __init__(self, drawingArea, settings):
+		'''
+		Constructor
+		drawingArea - where game stuff is drawn
+		'''
+
+		self.oDrawingArea = drawingArea
+		self.__width = settings['w'];
+		self.__height = settings['h'];
+		self.oDrawingArea.set_size_request(self.width, self.height)
+		self.__backgroundColor = [0.2, 0.3, 0.4]
+	pass
+
+	def draw(self):
+		'''
+		draws all objects on the screen
+		'''
+		cr = self._drawingArea.window.cairo_create()
+		cr.set_source_rgb(*self.__backgroundColor)
+		cr.rectangle(0, 0, self.width, self.height)
+		cr.fill()
+	pass
+
+	def _getHeight(self):
+		'''
+		get height of the canvas
+		'''
+		return self.__height
+	pass
+
+	def _getWidth(self):
+		'''
+		get width of the canvas
+		'''
+		return self.__width
+	pass
+
+	width = property(_getWidth, doc = "get width of the canvas")
+	height = property(_getHeight, doc = "get height of the canvas")
+pass
