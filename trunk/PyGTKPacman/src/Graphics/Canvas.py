5c18426b75453416d7decee4976b8c817c10a65b
Jar.Patryk <Jar.Patryk@1bd22912-60a4-6ab0-bb72-b58680a4c78c>
2010-04-11 12:52:37 +0000

    przeniesienie funkcjonalnosci zwiazanej z rysowaniem z WIndow do Canvas

--- trunk/PyGTKPacman/src/Graphics/Canvas.py
+++ trunk/PyGTKPacman/src/Graphics/Canvas.py
@@ -4,9 +4,16 @@ __author__="yarpo"
 __date__ ="$2010-04-11 13:04:47$"
 
 import pygtk
+import gtk
 pygtk.require("2.0")
 
-class Canvas(object):
+
+import math
+import random
+import gobject
+
+
+class Canvas(gtk.DrawingArea):
 	'''
 	class responsible for drawing stuff on the screen
 	'''
@@ -17,17 +24,57 @@ class Canvas(object):
 	__backgroundColor = []
 
 
-	def __init__(self, drawingArea, settings):
-		'''
-		Constructor
-		drawingArea - where game stuff is drawn
-		'''
+	def __init__(self):
+		gtk.DrawingArea.__init__(self);
+		self.__setUpMet()
+		gobject.idle_add(self.timeout)
+		#darea = gtk.DrawingArea()
+		self.connect("expose-event", self.expose)
+		#self.oDrawingArea = drawingArea
+		#self.__width = settings['w'];
+		#self.__height = settings['h'];
+		#self.set_size_request(self.width, self.height)
+		#self.__backgroundColor = [0.2, 0.3, 0.4]
+	pass
 
-		self.oDrawingArea = drawingArea
-		self.__width = settings['w'];
-		self.__height = settings['h'];
-		self.oDrawingArea.set_size_request(self.width, self.height)
-		self.__backgroundColor = [0.2, 0.3, 0.4]
+	def __setUpMet(self) :
+		# The number of circles and the window size.
+		self.num = 128
+		self.size = 512
+
+		# Initialize circle coordinates and velocities.
+		self.x = []
+		self.y = []
+		self.xv = []
+		self.yv = []
+		for i in range(self.num):
+			self.x.append(random.randint(0, self.size))
+			self.y.append(random.randint(0, self.size))
+			self.xv.append(random.randint(-4, 4))
+			self.yv.append(random.randint(-4, 4))
+		pass
+	pass
+
+	# Draw the circles and update their positions.
+	def expose(self, *args):
+		cr = self.window.cairo_create()
+		cr.set_line_width(4)
+		for i in range(self.num):
+			cr.set_source_rgb(1, 0, 0)
+			cr.arc(self.x[i], self.y[i], 8, 0, 2 * math.pi)
+			cr.stroke_preserve()
+			cr.set_source_rgb(1, 1, 1)
+			cr.fill()
+			self.x[i] += self.xv[i]
+			self.y[i] += self.yv[i]
+			if self.x[i] > self.size or self.x[i] < 0:
+				self.xv[i] = -self.xv[i]
+			if self.y[i] > self.size or self.y[i] < 0:
+				self.yv[i] = -self.yv[i]
+
+	def timeout(self):
+		self.queue_draw()
+		return True
 	pass
 
 	def draw(self):
