d295743b798f260f08da7055fe1028a39f9880b2
Jar.Patryk <Jar.Patryk@1bd22912-60a4-6ab0-bb72-b58680a4c78c>
2010-04-12 18:43:52 +0000

    wyswietlanie nieruchomych wrogów

--- PyGTKPacman/src/Game/Objects/Enemy.py
+++ PyGTKPacman/src/Game/Objects/Enemy.py
@@ -5,6 +5,8 @@ __version__ = "0.1";
 __author__  = "Patryk yarpo Jar <jar.patryk@gmail.com>";
 __date__    = "$2010-04-11 18:05:03$";
 
+from Character import *
+
 class Enemy(Character) :
 
 	def __init__(self, x, y):
